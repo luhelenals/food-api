a9a8d329cb050f4d91050c7128a023b153aa4113
Luiza <luizahelena.lsantos@gmail.com>
2025-01-19 22:26:47 -0300

    PUT receitas

--- api/controllers/ReceitaController.cs
+++ api/controllers/ReceitaController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using api.data;
 using api.dtos;
@@ -8,6 +9,7 @@ using api.mappers;
 using api.models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Extensions;
 
 namespace api.controllers
 {
@@ -53,6 +55,29 @@ namespace api.controllers
             _context.SaveChanges();
             
             return CreatedAtAction(nameof(GetById), new { id = receita.Id }, receita.ToReceitaDto());
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult EditReceita([FromBody] CreateReceitaRequestDto receitaDto, [FromRoute] int id)
+        {
+            Receita newReceita = receitaDto.ToReceitaFromCreateDto(_context);
+
+            var oldReceita = _context.Receitas.FirstOrDefault(x => x.Id == id);
+
+            if(oldReceita == null)
+                return NotFound();
+
+            if(newReceita.Ingredientes != null && newReceita.Ingredientes != oldReceita.Ingredientes)
+                oldReceita.Ingredientes = newReceita.Ingredientes;
+            
+            if(newReceita.Titulo != null && oldReceita.Titulo != newReceita.Titulo)
+                oldReceita.Titulo = newReceita.Titulo;
+            
+            oldReceita.Compatibilidade = newReceita.Compatibilidade;
+
+            _context.SaveChanges();
+
+            return Ok(oldReceita);
         }
     }
 }
