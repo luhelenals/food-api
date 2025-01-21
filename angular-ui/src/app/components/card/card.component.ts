5973fb9d1959f06a1ba8d5dc5a0180a3f535efd8
Luiza <luizahelena.lsantos@gmail.com>
2025-01-21 19:58:07 -0300

    cards scroll

--- angular-ui/src/app/components/card/card.component.ts
+++ angular-ui/src/app/components/card/card.component.ts
@@ -1,7 +1,6 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 // ingrediente => botão (em estoque => sem estoque)
-type CardVariants = "receita" | "ingrediente";
 
 @Component({
   selector: 'app-card',
@@ -12,7 +11,6 @@ type CardVariants = "receita" | "ingrediente";
 })
 export class CardComponent {
   @Input("card-title") cardTitle: string = "";
-  @Input() variant: CardVariants="receita";
   @Input("card-info") info: string = "";
 
   onCardClick():void {
